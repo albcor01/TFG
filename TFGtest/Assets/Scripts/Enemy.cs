2c4c8a4e6274eed1a94cce8ac55d3bdc3be040ac
Alberto <albcor01@ucm.es>
2019-10-07 13:17:00 +0200

    LLamadas a supercollider un poco mas interesantes
    con la tecla 'A' empieza una melodía de bajo y cuando te acercas a un enemigo suena una percusion

---
+++ TFGtest/Assets/Scripts/Enemy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    private const string ClientId = "SuperCollider";
+    private void OnTriggerEnter(Collider other)
+    {
+            float test = 1.0f;
+            OSCHandler.Instance.SendMessageToClient(ClientId, "/test2", test);
+    }
+}
---
+++ TFGtest/Assets/Scripts/test.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    private const string ClientId = "SuperCollider";
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            float test = 1.0f;
+            OSCHandler.Instance.SendMessageToClient(ClientId, "/test", test);
+            Debug.Log("hola");
+        }
+    }
+
+}

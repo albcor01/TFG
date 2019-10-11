86eb58b5088ae29825c79ef5878ad70efb48b3d4
Ramón <ramonarj@ucm.es>
2019-10-12 01:37:52 +0200

    Conseguido el acceso a las propiedades!!
    Ya funciona la detección de las propiedades de cada componente

--- TFGtest/Assets/Editor/PluginTFG.cs
+++ TFGtest/Assets/Editor/PluginTFG.cs
@@ -34,6 +34,7 @@ public class PluginTFG : EditorWindow
     //Listas de objetos y componentes que se van a usar
     List<Object> objetos = new List<Object>();
     List<List<Component>> componentes = new List<List<Component>>();
+    List<List<List<string>>> variables = new List<List<List<string>>>();
 
     //"Estilo" (es como el CSS del editor)
     //private static GUIStyle myStyle;
@@ -44,9 +45,9 @@ public class PluginTFG : EditorWindow
         GUILayout.Width(200)
     };
     //Botones para quitar
-    GUILayoutOption[] removeButtonsGUI = new GUILayoutOption[] 
+    GUILayoutOption[] removeGUI = new GUILayoutOption[] 
     {
-        GUILayout.Width(100)
+        GUILayout.Width(20)
     };
 
     //Selectores
@@ -54,10 +55,11 @@ public class PluginTFG : EditorWindow
     {
         GUILayout.Width(120)
     };
-    //Botones para quitar
-    GUILayoutOption[] removeComponentsGUI = new GUILayoutOption[]
+
+    //Selectores
+    GUILayoutOption[] addVariableGUI = new GUILayoutOption[]
     {
-        GUILayout.Width(120)
+        GUILayout.Width(80)
     };
 
     // Inicializa la ventana
@@ -76,7 +78,6 @@ public class PluginTFG : EditorWindow
         //    fontStyle = FontStyle.Bold
         //};
     }
-
     // Aquí es donde se hace todo lo importante
     void OnGUI()
     {
@@ -90,46 +91,86 @@ public class PluginTFG : EditorWindow
         //EditorGUILayout.EndToggleGroup();
 
         //Para nuestro plugin
-        GUILayout.Label("Música adaptativa", EditorStyles.boldLabel);
+        GUILayout.Label("Música adaptativa", EditorStyles.largeLabel);
 
         //Cada GameObject cuyos componentes queremos usar
         for (int i = 0; i < objetos.Count; i++)
         {
+            GUILayout.Label("Gameobject nº" + (i + 1), EditorStyles.boldLabel);
             EditorGUILayout.BeginHorizontal();
             objetos[i] = EditorGUILayout.ObjectField(objetos[i], typeof(GameObject), true, addObjectsGUI);
-            if (GUILayout.Button("Quitar objeto", removeButtonsGUI))
+
+            if (GUILayout.Button("X", removeGUI))
                 objetos.Remove(objetos[i]);
+
+            //Añadir un componente nuevo
+            if (GUILayout.Button("Añadir Componente", addComponentsGUI))
+            {
+                componentes[i].Add(new Component());
+                variables[i].Add(new List<string>());
+                //if (componentes[i].Capacity == componentes[i].Count)
+                //{
+                    
+                //}
+
+                //else
+                //    mensajeDebug = "Selecciona un componente para el input anterior";
+            }
             EditorGUILayout.EndHorizontal();
 
             //Cada componente cuyas variables queremos usar
             for (int j = 0; j < componentes[i].Count; j++)
             {
+                string cName = "Componente";
+                if (componentes[i][j] != null)
+                    cName = componentes[i][j].name;
+                GUILayout.Label(cName, EditorStyles.miniBoldLabel);
+
                 EditorGUILayout.BeginHorizontal();
                 componentes[i][j] = EditorGUILayout.ObjectField(componentes[i][j], typeof(Component), true, addComponentsGUI) as Component;
 
                 //Control de errores (comprobamos que el componente pertenece a ese gameobject)
-                if (componentes[i].Count > 0 && componentes[i][j] != null && componentes[i][j].gameObject  != objetos[i])
+                if (componentes[i].Count > 0 && componentes[i][j] != null && componentes[i][j].gameObject != objetos[i])
                 {
                     componentes[i][j] = null;
                     mensajeDebug = "Asigna un componente del GameObject seleccionado";
                 }
 
                 //Quitar un componente
-                if (GUILayout.Button("Quitar componente", removeComponentsGUI))
+                if (GUILayout.Button("X", removeGUI))
                     componentes[i].Remove(componentes[i][j]);
-                    
-                EditorGUILayout.EndHorizontal();
-            }
 
-            //Añadir un componente nuevo
-            if (GUILayout.Button("Añadir Componente", addComponentsGUI))
-            {
-                if(componentes[i].Capacity == componentes[i].Count)
-                    componentes[i].Add(new Component());
-                else
-                    mensajeDebug = "Selecciona un componente para el input anterior";
+                //Añadir una variable nueva
+                if (GUILayout.Button("Añadir variable", addVariableGUI))
+                {
+                    string s = "";
+                    variables[i][j].Add(s);
+                }
+
+                EditorGUILayout.EndHorizontal();
+
+                //Cada componente cuyas variables queremos usar
+                for (int k = 0; k < variables[i][j].Count; k++)
+                {
+                    EditorGUILayout.BeginHorizontal();
+                    variables[i][j][k] = EditorGUILayout.TextField(variables[i][j][k], addVariableGUI);
+
+                    //Quitar un componente
+                    if (GUILayout.Button("X", removeGUI))
+                        variables[i][j].Remove(variables[i][j][k]);
+
+                    EditorGUILayout.EndHorizontal();
+
+                    //TODO: TENER EN CUENTA MINUSCULAS Y MAYUSCULAS
+                    if(variables[i][j][k] != null)
+                    {
+                        if (MusicMaker.Utils.checkVariable(new MusicMaker.Variable(componentes[i][j], variables[i][j][k])))
+                            mensajeDebug = "Variable correcta";
+                        else
+                            mensajeDebug = "La variable " + variables[i][j][k] + " no se encuentra en el componente " + componentes[i][j];
+                    }
+                }
             }
-                
         }
 
         //Añadir un objeto nuevo
@@ -137,6 +178,7 @@ public class PluginTFG : EditorWindow
         {
             objetos.Add(new Object());
             componentes.Add(new List<Component>());
+            variables.Add(new List<List<string>>());
         }
 
 
--- TFGtest/Assets/Scripts/MMFoundation.cs
+++ TFGtest/Assets/Scripts/MMFoundation.cs
@@ -1,9 +1,85 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Reflection;
+using System.Linq;
 
 namespace MusicMaker
 {
+    public static class Utils
+    {
+        //Comprueba que la variable es correcta (es decir, que el componente tiene una propiedad que se llama como se indica)
+        public static bool checkVariable(Variable v)
+        {
+            object obj = v.component;
+            System.Type t = obj.GetType();
+            List<PropertyInfo>props = t.GetProperties().ToList();
 
+            //Propiedades del objeto (mirar C# reflection)
+            foreach (var prop in props)
+            {
+                //string s = "";
+                //if (prop.DeclaringType.Name == "MonoBehaviour")
+                //{
+                //    s = prop.Name + " (" + prop.PropertyType.Name + ") :" + prop.GetValue(obj);
+                //    Debug.Log(prop.DeclaringType.Name);
+                //}
+                //Debug.Log(prop.DeclaringType.Name);
+                //if (prop.Name == v.value)
+                // return true;
+                if (prop.Name == v.value)
+                    return true;
+            }
+            return false;
+        }
+    }
+    public class Variable
+    {
+
+        public Variable(Component component, string value)
+        {
+            this.owner = component.gameObject;
+            this.component = component;
+            this.value = value;
+        }
+
+
+        public Variable(Object owner, Component component, string value)
+        {
+            this.owner = owner;
+            this.component = component;
+            this.value = value;
+        }
+
+
+        public string value { get; set; }
+        public object component { get; set; }
+        public Object owner { get; set; }
+    }
+
+
+    public class ListaVariables
+    {
+        List<Variable> variables;
+
+
+        public ListaVariables()
+        {
+
+        }
+
+        public ListaVariables(List<Variable> variables)
+        {
+            this.variables = variables;
+        }
+
+
+        public void addVariable(Variable v)
+        {
+            //Metemos la variable
+            if (!variables.Contains(v))
+                variables.Add(v);
+        }
+
+    };
 }
-

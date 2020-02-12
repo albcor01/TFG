d73a4e6dce5272dbba3ecd29f627773815bd3eb8
Alberto <albcor01@ucm.es>
2020-02-12 19:13:05 +0100

    demo espacial creada

---
+++ TFGtest/Assets/Scenes/Demos/Spaceflight Controls/Scripts/EnemyCreator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCreator : MonoBehaviour
+{
+    bool EnCombate = false;
+    public GameObject Enemgo;
+    public GameObject EnemyShipRespawn;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!EnCombate) {
+            Invoke("InstanciaEnemigo", Random.Range(10.0f, 12.0f));
+            EnCombate = true;
+        }
+    }
+
+    public void EndBattle()
+    {
+        EnCombate = false;
+    }
+
+    void InstanciaEnemigo()
+    {
+        Vector3 pos;
+        if (EnemyShipRespawn.transform.position.y < gameObject.transform.position.y)
+            pos = new Vector3(EnemyShipRespawn.transform.position.x, EnemyShipRespawn.transform.position.y - 50, EnemyShipRespawn.transform.position.z); 
+        else pos = new Vector3(EnemyShipRespawn.transform.position.x, EnemyShipRespawn.transform.position.y + 50, EnemyShipRespawn.transform.position.z);
+        Instantiate(Enemgo, pos, Quaternion.identity);
+    }
+}
